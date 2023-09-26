bca4170fffbb6ccb31a45b42c16cff9b31777129
Karthik Maharaja AV <74762299+Karthik-Maharaja-AV@users.noreply.github.com>
2023-09-26 15:49:50 +0530

    Timer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Loading from './Components/Loading';
+
 
 function App() {
   return (
     <div className="w-full bg-back bg-cover">
       <Loading/>
+      
     </div>
   );
 }
--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import Title from "../Assets/header.svg";
 import College from "../Assets/college.svg";
+import CountdownTimer from "../CountdownTimer";
 
 const Loading = () => {
+
+  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+  const NOW_IN_MS = new Date().getTime();
+
+  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
+
+
+
   return (
     <div className="h-screen">
       <div className="flex h-4/5 items-center justify-center">
-        <div className="w-1/2">
-          
-        </div>
+        <div className="w-1/2"></div>
         <div className="flex h-full items-center justify-center flex-col">
-          <img src={Title}></img>
+          <img src={Title} alt="title"></img>
+          <h1>Countdown Timer</h1>
+          <CountdownTimer targetDate={dateTimeAfterThreeDays} />
         </div>
-        
       </div>
       <div className="flex items-end justify-end px-20">
-            <img src={College}></img>
-          </div>
-        
+        <img src={College} alt="college"></img>
+      </div>
     </div>
   );
 };
