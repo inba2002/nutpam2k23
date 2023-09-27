e0fb3329310ea5e1206f7f843e46acada8e306a1
Karthik Maharaja AV <74762299+Karthik-Maharaja-AV@users.noreply.github.com>
2023-09-27 11:24:30 +0530

    Font set Cursor set

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -3,14 +3,15 @@ import Title from "../Assets/header.svg";
 import College from "../Assets/college.svg";
 import CountdownTimer from "./CountdownTimer/CountdownTimer";
 import AnimatedCursor from "react-animated-cursor";
+import  '../App.css';
 
 const Loading = () => {
   return (
     <div className="h-screen pr-20">
-      <div className="flex h-4/5 pt-10 items-center justify-center">
+      <div className="md:flex h-4/5 pt-10 items-center justify-center">
         <div className="w-1/2"></div>
         <div className="flex h-full items-center gap-10 justify-center flex-col">
-          <img src={Title} alt="title"></img>
+          <img className="w-full" src={Title} alt="title"></img>
           <CountdownTimer CountdownTimeStampMs={1697623200000} />
         </div>
       </div>
@@ -19,36 +20,16 @@ const Loading = () => {
         <img src={College} alt="college"></img>
       </div>
       <AnimatedCursor
-        innerSize={40}
-        outerSize={40}
+        innerSize={100}
+        outerSize={100}
         color="193, 11, 111"
         outerAlpha={0.3}
         innerScale={0.7}
-        outerScale={5}
-        clickables={[
-          "a",
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          "label[for]",
-          "select",
-          "textarea",
-          "button",
-          ".link",
-          {
-            target: ".custom",
-            options: {
-              innerSize: 12,
-              outerSize: 12,
-              color: "255, 255, 255",
-              outerAlpha: 0.3,
-              innerScale: 0.7,
-              outerScale: 5,
-            },
-          },
-        ]}
+        outerScale={0}
+        innerStyle={{
+          mixBlendMode: 'exclusion',
+          backdropFilter: 10
+        }}
       />
     </div>
   );
