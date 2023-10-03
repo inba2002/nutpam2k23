60f135e240bf76aa7fffd5123556728155b8d96d
Karthik Maharaja AV <74762299+Karthik-Maharaja-AV@users.noreply.github.com>
2023-10-04 00:04:15 +0530

    navbar corrected

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -25,8 +25,33 @@ export const navLinks = [
 const Navbar = () => {
   return (
     <div className="w-screen">
-      <div className="navbar navbar-end bg-transparent w-full  md:px-96">
-        <div className="navbar-end">
+      <div className="navbar navbar-end bg-transparent w-full h-32">
+        <div className="w-full flex items-center justify-end">
+          <div className="flex w-1/2 items-center justify-center">
+          <ul
+              
+                className="md:flex hidden border-b-2 border-white/25 items-center justify-between px-10 text-white gap-10 text-xl p-2  w-full"
+              >
+                <li className="hover:text-white/50 w-24 h-10 hover:bg-white/25 flex items-center justify-center ">
+                  <a>Home</a>
+                </li>
+                <li className="hover:text-white/50 w-24 h-10 hover:bg-white/25 flex items-center justify-center">
+                  <a>About</a>
+                </li>
+                <li className="hover:text-white/50 w-24 h-10 hover:bg-white/25 flex items-center justify-center">
+                  <a>Mission</a>
+                </li>
+                <li className="hover:text-white/50 w-24 h-10 hover:bg-white/25 flex items-center justify-center">
+                  <a>Sponsers</a>
+                </li>
+                <li className="hover:text-white/50 w-24 h-10 hover:bg-white/25 flex items-center justify-center">
+                  <a>Team</a>
+                </li>
+                <li className="hover:text-white/50 w-24 h-10 hover:bg-white/25 flex items-center justify-center">
+                  <a>Contact</a>
+                </li>
+              </ul>
+          </div>
           <div className="dropdown md:hidden">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
               <svg
@@ -44,19 +69,28 @@ const Navbar = () => {
                 />
               </svg>
             </label>
-            <div className="flex items-center justify-center">
+            <div className="flex items-center  justify-center">
               <ul
                 tabIndex={0}
-                className="menu items-center  menu-md dropdown-content mt-28 z-[1] p-2 shadow pr-96 rounded-box w-52"
+                className="menu items-center  menu-lg dropdown-content mt-64 z-[1] mr-32 p-2 border border-white/10 shadow-xl pr-96 rounded-box w-28"
               >
                 <li>
-                  <a>Homepage</a>
-                </li>
-                <li>
-                  <a>Portfolio</a>
+                  <a>Home</a>
                 </li>
                 <li>
                   <a>About</a>
+                </li>
+                <li>
+                  <a>Mission</a>
+                </li>
+                <li>
+                  <a>Sponsers</a>
+                </li>
+                <li>
+                  <a>Team</a>
+                </li>
+                <li>
+                  <a>Contact</a>
                 </li>
               </ul>
             </div>
--- src/discover.js
+++ src/discover.js
@@ -1,18 +1,51 @@
 import Discovers from "./Assets/discover.png";
-
+import Arrow from "./Assets/double arrow.svg";
+import DisNut from "./Assets/DISCOVER NUTPAM.png";
+import Heading from "./Assets/Heading.png";
+import './App.css';
 
 export default function Discover() {
   return (
-    <div className="flex items-center justify-center w-screen p-10">
+    <div className="flex items-center justify-center w-screen p-14">
       <div className="md:flex items-center justify-center">
         <img src={Discovers} className="md:w-1/4"></img>
-        <p className="md:text-2xl md:w-1/2 indent-8 text-white text-justify">
-          Nutpam 2k22 is created with the theme of “The Mighty Ocean”. With a
-          simple, yet intriguing theme, we are obliged to make an indulging
-          knowledge pool for all the engineers, just like the mighty ocean with
-          exciting and innovative events designed to be fun, entertaining, and
-          hectic at the same time.{" "}
-        </p>
+        <div className="flex flex-col items-center justify-center md:w-1/2 gap-10">
+          <div className="flex items-center justify-center gap-8 text-white text-2xl w-auto rounded bg-white/[0.2] p-4">
+            <svg
+              className="hidden md:block"
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="31"
+              viewBox="0 0 26 25"
+              fill="none"
+            >
+              <g opacity="0.9">
+                <path
+                  d="M13.4987 19.792L6.20703 12.5003L13.4987 5.20866"
+                  stroke="white"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M19.7487 19.792L12.457 12.5003L19.7487 5.20866"
+                  stroke="white"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </g>
+            </svg>
+            <img src={DisNut} className="md:w-auto w-42"></img>
+          </div>
+          <p className="md:text-2xl md:w-3/4 w-full indent-8 tex text-white text-justify">
+            Nutpam 2k22 is created with the theme of “The Mighty Ocean”. With a
+            simple, yet intriguing theme, we are obliged to make an indulging
+            knowledge pool for all the engineers, just like the mighty ocean
+            with exciting and innovative events designed to be fun,
+            entertaining, and hectic at the same time.{" "}
+          </p>
+        </div>
       </div>
     </div>
   );
